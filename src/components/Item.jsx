f9030ef2fe3684e9470a10e06ba1cccc9a9a656c
Jose Carlos <josecarlos.jcop.3@gmail.com>
2021-05-09 18:58:05 -0400

    Added format in to code

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import './Item.css'
-import PropTypes from 'prop-types'
+import React from "react";
+import "./Item.css";
+import PropTypes from "prop-types";
 
-const Item = ({item}) => {
-
-  return (
-    <div className="row">
-      <div className="column">
-        <div className="card">
-          <img src={item.url} alt={item.name} width='250px' height='250px'/>
-          <h3>{item.name}</h3>
-          <p>{item.age}</p>
-          <p>{item.team}</p>
-        </div>
-      </div>
-    </div>
-  );
+const Item = ({ item }) => {
+	return (
+		<div className="row">
+			<div className="column">
+				<div className="card">
+					<img src={item.url} alt={item.name} width="250px" height="250px" />
+					<h3>{item.name}</h3>
+					<p>{item.age}</p>
+					<p>{item.team}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 Item.propTypes = {
-  item: PropTypes.object.isRequired
+	item: PropTypes.object.isRequired
 };
 
 export default Item;
